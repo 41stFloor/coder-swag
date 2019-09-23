a90f9524519ee2825f82b10bdc9ed9ab8e2272ac
Jonah Travis <master@Jonahs-iMac.fios-router.home>
2019-09-23 16:13:48 -0400

    Adding Cateogy table

---
+++ coder-swag/coder-swag/Views/CategoryCell.swift
@@ -0,0 +1,29 @@
+//
+//  CategoryCell.swift
+//  coder-swag
+//
+//  Created by Jonah Travis on 9/23/19.
+//  Copyright © 2019 Jonah Travis. All rights reserved.
+//
+
+import UIKit
+
+class CategoryCell: UITableViewCell {
+    
+    @IBOutlet weak var categoryImage: UIImageView!
+    @IBOutlet weak var categoryTitle: UILabel!
+    
+    
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
