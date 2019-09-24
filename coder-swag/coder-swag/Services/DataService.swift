8b5de03af3cfcbf8a3716d9c5aee218e3fa1e569
Jonah Travis <master@Jonahs-iMac.fios-router.home>
2019-09-23 23:11:06 -0400

    Added additional objects in table view. For some reason it fucked up the view but nothing on the internet seems to describe what went wrong.

---
+++ coder-swag/coder-swag/Services/DataService.swift
@@ -0,0 +1,24 @@
+//
+//  DataService.swift
+//  coder-swag
+//
+//  Created by Jonah Travis on 9/23/19.
+//  Copyright © 2019 Jonah Travis. All rights reserved.
+//
+
+import Foundation
+
+class DataService {
+    static let instance = DataService()
+    
+    private let categories = [
+        Category(title: "SHIRTS", imageName: "shirts.png"),
+        Category(title: "HOODIES", imageName: "hoodies.png"),
+        Category(title: "HATS", imageName: "hats.png"),
+        Category(title: "DIGITAL", imageName: "digital.png")
+    ]
+    
+    func getCategories() -> [Category] {
+        return categories
+    }
+}
