2715aff55bb60d2460b744c445f0815deb668320
Jonah Travis <master@Jonahs-iMac.fios-router.home>
2019-09-23 23:38:56 -0400

    Custom collection view cell.

---
+++ coder-swag/coder-swag/Views/ProductCell.swift
@@ -0,0 +1,16 @@
+//
+//  ProductCell.swift
+//  coder-swag
+//
+//  Created by Jonah Travis on 9/23/19.
+//  Copyright © 2019 Jonah Travis. All rights reserved.
+//
+
+import UIKit
+
+class ProductCell: UICollectionViewCell {
+    @IBOutlet weak var productImage: UIImageView!
+    @IBOutlet weak var productTitle: UILabel!
+    @IBOutlet weak var productPrice: UILabel!
+    
+}
